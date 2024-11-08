abe9d918fa2053f4e882b550d8ae5fdaf68a5aa4
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-11-08 03:58:55 +0000

    feat: Adding kernel loading to the master boot record.

--- src/master-boot-record.asm
+++ src/master-boot-record.asm
@@ -8,3 +8,24 @@ mov [BOOT_DRIVE], dl
 
 mov bp, 0x9000
 mov sp, bp
+
+
+
+call load_kernel
+call switch_to_32bit
+jmp $
+
+
+%include "disk.asm"
+%include "global-descriptor-table.asm"
+%include "switch-to-32bit.asm"
+
+
+
+[bits 16]
+load_kernel:
+    mov bx, KERNEL_OFFSET
+    mov dh, 2
+    mov dl, [BOOT_DRIVE]
+    call disk_load
+    ret
